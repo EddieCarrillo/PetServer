cdbf92a05ad67736c6c6dc1807d68dbdbbf881ad
EddieCurio <edcarril@ucsd.edu>
2017-08-21 15:51:55 -0700

    Hooked up to db

--- server/api/pet/petController.js
+++ server/api/pet/petController.js
@@ -1,29 +1,81 @@
+var Pet = require('./petModel');
+var _ = require('lodash');
+
+
+
+
+
+
 
 exports.param = function(req, res, next, id){
-  req.id = id;
-  console.log("id path param used!");
-  if (id == 'me'){
-    console.log("me path param used.")
-  }
-  next();
+  Pet.findOne({_id: id}, function(err, pet){
+    if(err){
+      console.log(err);
+      res.status(400).json();
+    }else if(!pet){
+      console.log("Could not find the user.")
+      res.status(400).json();
+    }else{
+      res.pet = pet;
+      next();
+    }
+  });
 }
 
 exports.get = function(req, res){
-  console.log("returns all public pets");
-  res.json();
+  Pet.find({}, function (err, pets){
+    if (err){
+      console.log("ERROR", err.message);
+      res.status(400).json();
+    }else if (!pets){
+      console.log("No pets found!!!");
+      res.status(400).json();
+    }else{
+      console.log("Return list of pets", pets);
+      res.status(200).json(pets);
+    }
+  });
 }
 
 exports.getId =   function(req, res, next){
   console.log("returns a pet represented by its id");
-  res.json();
+  res.json(res.pet);
 }
 
 exports.put = function(req, res){
-  console.log("Update the pet with the id of..." + req.id);
-  res.json();
+  var pet = res.pet
+  var update = req.body;
+
+  _.merge(pet, update);
+  console.log(pet);
+
+  pet.save(function(err, pet){
+    if (err){
+      console.log("Error", err.message);
+      res.status(400).json();
+    }else if (!pet){
+      console.log("Not able to find pet");
+      res.status(400).json();
+    }else{
+      console.log("Update the pet");
+      res.status(200).json(pet);
+    }
+  })
+
 }
 
 exports.post = function(req, res){
-  console.log("Creates and retuns a new pet using the posted object as the pet");
-  res.json();
+  var newPet = req.body;
+
+  Pet.create(newPet, function(err, createdPet){
+    if (err){
+      console.log("ERROR", err.message);
+      res.status(400).json();
+    }else if (!createdPet){
+      console.log("Could not create a pet!")
+      res.status()
+    }else{
+      res.status(201).json(createdPet);
+    }
+  })
 }
