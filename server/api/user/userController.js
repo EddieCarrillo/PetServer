b404011d12647dc50995759279fa6a880b1c58be
EddieCurio <edcarril@ucsd.edu>
2017-08-21 16:20:04 -0700

    Connected to db

--- server/api/user/userController.js
+++ server/api/user/userController.js
@@ -1,22 +1,50 @@
-
+var User = require('./userModel');
 
 exports.param = function(req, res, next, id){
-  console.log("Path param found: " + id);
-  res.json();
+  User.findOne({_id: id}, function(err, user){
+    if (err){
+      console.log(err);
+      res.status(400).json();
+    }else if (!user){
+      console.log("Could not find user with id.");
+      res.status(400).json();
+    }else{
+      res.user = user
+      next();
+    }
+  })
 }
 
 
 exports.get = function(req, res){
-  console.log("Get all the public users");
-  res.json();
+  User.find({}, function(err, users){
+    if (err){
+      console.log("ERROR",err);
+      res.status(400).json();
+    }else if (!users){
+      console.log("Could not find users");
+      res.status(400).json();
+    }else{
+      res.status(200).json(users);
+    }
+  })
 }
 
 exports.getId =  function(req, res){
-  console.log("Return a user with id: " + req.params.id);
-  res.json();
+  res.json(res.user);
 }
 
 exports.post = function(req, res){
-  console.log("Create a new user");
-  res.json();
+  User.create(req.body, function(err, user){
+    if (err){
+      console.log(err);
+      res.status(400).json();
+    }else if (!user){
+      console.log("Could not create user");
+      res.status(400).json();
+    }else{
+      res.status(201).json(user);
+    }
+  });
+
 }
