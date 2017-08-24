d6fbfbe5dae56c250b652f21b2aaead65e34c3dc
EddieCurio <edcarril@ucsd.edu>
2017-08-23 18:04:22 -0700

    Moved upload router callback to controller

---
+++ server/api/files/filesRouter.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var filesController = require('./filesController');
+var multer = require('multer');
+var stream  = require('stream');
+var upload = multer();
+var gfs = require('../../db');
+
+router.post('/upload', upload.single('file'), filesController.upload)
+router.param('fileName', filesController.param);
+router.get('/:fileName', filesController.get);
+router.post('/', filesController.post);
+
+module.exports = router
