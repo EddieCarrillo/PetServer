1954982e172099ccb67637e36c12a93582910217
EddieCurio <edcarril@ucsd.edu>
2017-08-23 18:02:52 -0700

    Added GridFS implementation

--- server/api/files/filesController.js
+++ server/api/files/filesController.js
@@ -1,23 +1,74 @@
+var multer = require('multer');
+var upload = multer();
+var mongoose = require('mongoose');
+var gfs = require('../../db');
+var fs = require('fs');
+var stream = require('stream');
+var crypto = require('crypto');
 var Files = require('./filesModel');
 
 
+
 exports.param = function(req, res, next, fileName){
-    Files.findOne({name: fileName}, function(err, file){
-      if (err){
-         console.log("Error had occured.", err.message);
-        res.status(400).json();
-      }else if (!file){
-        console.log("Could not find the file.")
-        res.status(400).json();
-      }else {
-        res.file = file;
-        next();
-      }
+    var readStream = gfs.createReadStream({
+      filename: fileName
     });
+
+    readStream.on('error', function(err){
+      console.log(err.message);
+      return  res.status(400).json();
+    });
+
+    if (!readStream){
+    return  res.status(400).json();
+    }
+
+    req.stream = readStream
+    next();
+
+
 }
 
+exports.upload = function(req, res, next ){
+  var file = req.file
+  console.log("HEADERS:",req.headers)
+  console.log("BODY", req.body);
+  if (!file){
+    console.log('Trouble finding file wanted to upload');
+    return res.status(401).json();
+  }
+ console.log(file)
+
+//Convert file buffer to stream to be passed to GridFS
+  var buffer = req.file.buffer;
+  var uploadFileStream = new stream.PassThrough();
+  uploadFileStream.end(buffer);
+  var id = crypto.randomBytes(20).toString();
+
+  var writeStream = gfs.createWriteStream({
+    filename: file.originalname
+  });
+
+  uploadFileStream.pipe(writeStream);
+
+  writeStream.on('close', function(){
+    console.log("Finished writing to GRIDFS");
+  });
+
+  writeStream.on('error', function(){
+     console.log("Error writing file to GRID");
+    return res.status(400).json();
+  })
+
+  res.status(201).json({ok: "ok"});
+
+}
+
+
+
 exports.get = function(req, res, next){
-    res.json(res.file);
+    req.stream.pipe(res);
+
 }
 
 exports.post = function(req, res, next){
