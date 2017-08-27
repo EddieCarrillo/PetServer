9f01a18c84ea913d27a9ac3439337c69049bf0e9
EddieCurio <edcarril@ucsd.edu>
2017-08-26 19:04:53 -0700

    Added comment and file routers

--- server/api/api.js
+++ server/api/api.js
@@ -4,13 +4,16 @@ var router = express.Router();
 var userRouter = require('./user/userRouter');
 var petRouter = require('./pet/petRouter');
 var postRouter = require('./post/postRouter');
-var commentRouter = require('./comment/commentRouter')
+var commentRouter = require('./comment/commentRouter');
+var fileRouter = require('./files/filesRouter');
 
 
 router.use('/users', userRouter);
 router.use('/pets', petRouter);
 router.use('/posts', postRouter);
 router.use('/pets', petRouter);
+router.use('/comments', commentRouter);
+router.use('/files', fileRouter); 
 
 
 module.exports = router
