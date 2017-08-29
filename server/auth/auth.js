3c408c090aec236300e88851a195901846ac7b44
EddieCurio <edcarril@ucsd.edu>
2017-08-28 20:49:47 -0700

    Added access control for owner of pet

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,8 +1,8 @@
 var jwt = require('jsonwebtoken');
 var expressJwt = require('express-jwt')
 var config = require('../config/config')
-var checkValidToken = expressJwt({secret: config.secrets.jwt});
-var User = require('../api/user/userModel');
+var checkValidToken = expressJwt({secret: config.secrets.jwt})
+var User = require('../api/user/userModel')
 
 exports.decodeToken = function(){
   return function(req, res, next){
@@ -11,6 +11,7 @@ exports.decodeToken = function(){
     // if (req.query && req.query.hasOwnProperty('access_token')) {
     //   req.headers.authorization = 'Bearer ' + req.query.access_token;
     // }
+    console.log('decodeToken()')
     checkValidToken(req, res ,next)
   }
 }
@@ -18,36 +19,54 @@ exports.decodeToken = function(){
 
 exports.getFreshUser = function(){
   return function(req, res, next){
-
-  }
-
-}
-
-
-
-
-exports.verifyUser = function(req, res, next){
-  return function(req, res, next){
-    var user = req.user
-    var username = user.username
-    var password = user.password
-
-
-    if (!username || !password){
-      console.log("Missing username and/or password")
-    return  res.status(400).send("Missing username and/or password");
-    }
-
-    User.find(user, function(err, foundUser){
+    console.log('getFreshUser()')
+    User.findById(req.user._id, function(err, user){
       if (err){
-        console.log('Could not find user!')
+        console.log('ERROR', err.message);
+        return res.status(400).send('Err finding user in database');
+      }else if (!user){
+        //Deleted user because weird stuff or asian with beard and ponytail trying to hack with jwt
+        return res.status(400).send('User does not exist in the database');
+      }else{
+        req.user = user;
+        next()
       }
     })
 
 
   }
+
 }
 
+//Checks to verify the username and password of the user
+//If failure then sends back error status
+//If succesful then next() is called.
+exports.verifyUser = function(){
+  return function(req, res, next){
+
+    var user = req.body
+    var username = user.username
+    var plainTextPassword = user.password
+    //console.log('verifyUser()')
+
+
+
+    if (!username || !plainTextPassword){
+      console.log("Missing username and/or password")
+    return  res.status(400).send("Missing username and/or password");
+    }
+
+  User.findOne({username: username})
+   .then(function(foundUser){
+    if (!foundUser){
+      res.status(400).send('No user with that username')
+    }else{
+      foundUser.authenticate(plainTextPassword, authenticateResultHandler(req,res, next, foundUser))
+    }
+  })
+
+}
+}
 
 exports.signToken = function(id) {
   return jwt.sign(
@@ -56,3 +75,18 @@ exports.signToken = function(id) {
     {expiresIn: config.expireTime}
   );
 };
+
+
+var authenticateResultHandler = function(req,res, next, user){
+  return function(err, result){
+    if (err){
+      res.status(400).send('Error authenticate')
+    }else if (!result){
+      res.status(400).send('Bad password')
+    }else{
+      console.log('Good password  ')
+      req.user = user
+      next()
+    }
+  }
+}
