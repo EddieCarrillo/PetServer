02fde37ea98fcb1af7c9ec4e61e63825479db78e
EddieCurio <edcarril@ucsd.edu>
2017-08-31 08:18:43 -0700

    Added application scope level error handling middleware. Also added authentication router

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
 var express = require('express');
 var app = express();
 var apiRouter = require('./api/api');
+var authRouter = require('./auth/router');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt')
+var checkValidToken = expressJwt({secret: 'lemon'})
+
+mongoose.connect('mongodb://localhost/myapp');
+
+
+
+app.post('/test', function(req,res, next){
+
+
+});
+
 //For logging
 app.use(morgan());
 //Json parsing
 app.use(bodyParser.json());
 //Set up api routes
 app.use('/api', apiRouter );
+app.use('/auth', authRouter);
+
+app.use(function(err, req, res, next){
+  console.log(err.message)
+  res.status(400).send('Here is the error ^^^^^^')
+});
+
 
 
 
@@ -20,3 +42,6 @@ app.use('/api', apiRouter );
 app.listen(3000, function(){
   console.log("Listening on port 3000.");
 });
+
+
+module.exports = app;
