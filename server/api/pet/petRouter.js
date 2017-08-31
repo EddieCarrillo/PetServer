6b4f8a5a4284092e74eac64aaf4d46ccdb62e0df
EddieCurio <edcarril@ucsd.edu>
2017-08-31 07:59:39 -0700

    Added access control middleware. Added authentication middleware. Added route for getting all a single users pet's

--- server/api/pet/petRouter.js
+++ server/api/pet/petRouter.js
@@ -1,17 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var petController = require('./petController');
+var decodeToken = require('../../auth/auth').decodeToken;
+var getFreshUser = require('../../auth/auth').getFreshUser;
+//Access control middleware
+var  ownsPet = function(){
+  return function(req, res, next){
+    console.log('ownsPet()')
+    var pet = req.pet
+
+    if (String(pet.owner) == String(req.user._id)){
+      console.log('user owns this pet!')
+      next();
+    }else{
+      console.log('User does not have access to this resource.')
+      res.status(400).send('This is not you pet bitch.')
+
+      console.log('Actual owner: ',   pet.owner)
+      console.log('Received user: ',   req.user._id)
+    }
+  }
+}
+
+var checkUser = [decodeToken(), getFreshUser(), ownsPet()];
+
+
+
 
 
 router.param('id', petController.param);
 
-router.get('/', petController.get);
+router.route('/')
+.get(petController.get)
+.post(decodeToken(), getFreshUser(),petController.post)
 
-router.get('/:id', petController.getId);
 
-router.put('/:id', petController.put);
+//Edit ability needs authorization
+router.route('/:id')
+.put(checkUser,petController.put)
+.get(petController.getId)
 
-router.post('/', petController.post);
+router.route('/me')
+.get(decodeToken(), getFreshUser(), petController.getMe)
+
+
+
 
 
 
