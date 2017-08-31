7d304c209c75a5b605f5152bda3da2f82f9f8cda
EddieCurio <edcarril@ucsd.edu>
2017-08-31 07:52:31 -0700

    Changed gridfs require

--- server/api/files/filesRouter.js
+++ server/api/files/filesRouter.js
@@ -4,7 +4,7 @@ var filesController = require('./filesController');
 var multer = require('multer');
 var stream  = require('stream');
 var upload = multer();
-var gfs = require('../../db');
+var gfs = require('../../db').gridfs;
 
 router.post('/upload', upload.single('file'), filesController.upload)
 router.param('fileName', filesController.param);
