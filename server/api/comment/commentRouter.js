b7655a8ec0e18f3e3e04d44e3a4a056b00d3ab24
EddieCurio <edcarril@ucsd.edu>
2017-08-31 07:50:48 -0700

    Refactor router.method -> router.route.method

--- server/api/comment/commentRouter.js
+++ server/api/comment/commentRouter.js
@@ -5,10 +5,15 @@ var router = express.Router();
 
 router.param('id', commentController.param);
 
-router.get("/", commentController.get );
+router.route('/')
+.get(commentController.get)
+.post(commentController.post)
 
-router.get('/:id', commentController.getId );
+router.route('/:id')
+.get(commentController.getId)
 
-router.post('/', commentController.post);
+
+
+
 
 module.exports = router;
