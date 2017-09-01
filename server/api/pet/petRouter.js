98d040ce173d76621254f04f1e47c047982a1077
EddieCurio <edcarril@ucsd.edu>
2017-08-31 19:13:47 -0700

    Added query params extractor middleware

--- server/api/pet/petRouter.js
+++ server/api/pet/petRouter.js
@@ -3,21 +3,22 @@ var router = express.Router();
 var petController = require('./petController');
 var decodeToken = require('../../auth/auth').decodeToken;
 var getFreshUser = require('../../auth/auth').getFreshUser;
+var queryExtractor = require('../../middleware.js').prepareQuery
 //Access control middleware
 var  ownsPet = function(){
   return function(req, res, next){
-    console.log('ownsPet()')
+    // console.log('ownsPet()')
     var pet = req.pet
 
     if (String(pet.owner) == String(req.user._id)){
-      console.log('user owns this pet!')
+      // console.log('user owns this pet!')
       next();
     }else{
       console.log('User does not have access to this resource.')
       res.status(400).send('This is not you pet bitch.')
 
-      console.log('Actual owner: ',   pet.owner)
-      console.log('Received user: ',   req.user._id)
+      // console.log('Actual owner: ',   pet.owner)
+      // console.log('Received user: ',   req.user._id)
     }
   }
 }
@@ -31,7 +32,7 @@ var checkUser = [decodeToken(), getFreshUser(), ownsPet()];
 router.param('id', petController.param);
 
 router.route('/')
-.get(petController.get)
+.get(queryExtractor(), petController.get)
 .post(decodeToken(), getFreshUser(),petController.post)
 
 
@@ -40,8 +41,7 @@ router.route('/:id')
 .put(checkUser,petController.put)
 .get(petController.getId)
 
-router.route('/me')
-.get(decodeToken(), getFreshUser(), petController.getMe)
+
 
 
 
