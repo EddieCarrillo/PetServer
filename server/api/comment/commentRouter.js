67213f577d4102aa864af4999dc44837c0e20837
EddieCurio <edcarril@ucsd.edu>
2017-09-05 19:18:21 -0700

    Protected routes and added query extractor to routes

--- server/api/comment/commentRouter.js
+++ server/api/comment/commentRouter.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var commentController = require("./commentController");
+var decodeToken = require('../../auth/auth').decodeToken;
+var getFreshUser = require('../../auth/auth').getFreshUser;
+var queryExtractor = require('../../middleware.js').prepareQuery
 
 var router = express.Router();
 
 router.param('id', commentController.param);
 
 router.route('/')
-.get(commentController.get)
-.post(commentController.post)
+.get(queryExtractor(), commentController.get)
+.post(decodeToken(), getFreshUser(), commentController.post)
 
 router.route('/:id')
 .get(commentController.getId)
