dc53e9611f5cf69f47fbd5f4988d66bd80942a00
EddieCurio <edcarril@ucsd.edu>
2017-09-13 16:23:19 -0700

    Added route to check auth token validity

--- server/auth/controller.js
+++ server/auth/controller.js
@@ -1,12 +1,17 @@
 var User = require('../api/user/userModel');
 var signToken = require('./auth').signToken
+ var config = require('../config/config')
 
 
 
 exports.login = function(req, res, next){
 //Send back a token if user is verifu
-  var token = signToken(req.user._id)
+  var token = signToken(req.user._id)	
   res.json({token: token})
 
 
+}
+
+exports.checkValidToken = function(req, res, next){
+	res.status(200).send(req.user)
 }
