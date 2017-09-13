9282ce46320f2bb05f90905e2be63a8ceffdd0d4
EddieCurio <edcarril@ucsd.edu>
2017-09-13 16:24:57 -0700

    Added middleware to retrieve posted auth token and place as the auth header

--- server/auth/router.js
+++ server/auth/router.js
@@ -2,15 +2,40 @@ var express = require('express');
 var router = express.Router();
 var User = require('../api/user/userModel')
 var controller = require('./controller')
+var decodeToken = require('./auth').decodeToken
 var verifyUser = require('./auth').verifyUser
+var getFreshUser = require('./auth').getFreshUser
+
+var extractToken  = function(){
 
 
 
+	return function(req, res, next){
+		//If the token was received then put it as the authorization header
+	if (req.body){
+		console.log("req.body: " + JSON.stringify(req.body))
+		var token = req.body.token
+		if (token){
+			console.log("token: " + token)
+			req.headers.authorization = 'Bearer ' + token
+		}else {
+			res.status(401).send('Could not find token field')
+		}
+	}else {
+		res.status(401).send('Could not get the response body')
+	}
+	    next()
 
 
+	   }
+	}
+	
 
 //Req.user will be there from middleware
 router.post('/login', verifyUser(), controller.login );
+
+//
+router.post('/checkValidToken', extractToken(), decodeToken(), getFreshUser(), controller.checkValidToken)
 
 
 
