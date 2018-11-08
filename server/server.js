13e0fc0cfe98c628d26d3f7b583acab5d5f46751
Eduardo Carrillo <edcarril@ucsd.edu>
2018-11-08 13:55:56 -0800

    Changed the casing for the port env var

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next){
 
 
 
-app.listen(process.env.port || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Listening on port");
 });
 
